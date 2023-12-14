4f1552afadc35420356ce827f8bc38109329859f
bartdurak <bartedr@hotmail.com>
2023-12-14 20:55:41 +0100

    02 (mainmod)

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+// Created by fe36 on 27/12/22.
+//
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 12;
+    int y = 3;
+    cout << x / y;
+    return 0;
+}//
+// Created by fe36 on 27/12/22.
+//
+//
+// Created by fed36 on 09.01.23.
+//
